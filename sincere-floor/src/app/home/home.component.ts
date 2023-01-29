96f600e565e78cbf078ac5102450c667c9f3f0f1
mahanbeom <akgksqja@gmail.com>
2023-01-29 20:19:21 +0900

    design: Add About US

--- sincere-floor/src/app/home/home.component.ts
+++ sincere-floor/src/app/home/home.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import SwiperCore, { Navigation, EffectCoverflow, Pagination, Mousewheel, Autoplay } from "swiper";
+
+SwiperCore.use([Autoplay, Navigation, EffectCoverflow, Pagination, Mousewheel]);
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+	selector: 'app-home',
+	templateUrl: './home.component.html',
+	styleUrls: ['./home.component.scss'],
+	encapsulation: ViewEncapsulation.None
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+
+	imgList: string[] = [];
+
+	ngOnInit(): void {
+		this.loadImages();
+	}
+
+	private loadImages() {
+		for (let i = 0; i < 6; i++) {
+			this.imgList[i] = `../../assets/images/home_img${i + 1}.jpg`;
+		}
+	}
 
 }
--- sincere-floor/src/app/shared.module.ts
+++ sincere-floor/src/app/shared.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { SwiperModule } from 'swiper/angular';
 
 // material modules
 import { MatButtonModule } from '@angular/material/button';
@@ -30,6 +31,7 @@ const materialModules = [
 
 const modules = [
     HttpClientModule,
+    SwiperModule,
     ...materialModules
 ];
 
